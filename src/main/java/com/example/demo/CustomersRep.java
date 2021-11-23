daa1a3f5356ef3904ba14027fe13ac083a52309f
Eswar <eswar.banala@gmail.com>
2021-11-23 21:02:52 +0530

    REST API done

--- src/main/java/com/example/demo/CustomersRep.java
+++ src/main/java/com/example/demo/CustomersRep.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomersRep extends CrudRepository<Customers, Integer>{
+public interface CustomersRep extends JpaRepository<Customers, Integer>{
 
 }
--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -41,6 +49,71 @@ public class FormController {
 		mv.addObject(customers);
 		return mv;
 	}
+
+	/*
+	@RequestMapping("/customers")
+	@ResponseBody
+	public String getCustomers()
+	{
+		return repo.findAll().toString();
+	}
+	*/
+	/*
+	@RequestMapping("/customers/2")
+	@ResponseBody
+	public String getCustomers2()
+	{
+		return repo.findAll().toString();
+	}
+	*/
 	
+	@RequestMapping("/customers")
+	@ResponseBody
+	//for this the output is in JSON format
+	public List<Customers> getCustomers()
+	{
+		return repo.findAll();
+	}
+	
+	/*
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public String getCustomers2(@PathVariable("cid") int cid)
+	{
+		//return repo.findAll().toString();
+		return repo.findById(cid).toString();
+	}
+	*/
+	
+	//for this the output is in JSON format
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public Optional<Customers> getCustomers2(@PathVariable("cid") int cid)
+	{
+		//return repo.findAll().toString();
+		return repo.findById(cid);
+	}
+	
+	@PostMapping("/customers")
+	public Customers getCustomers3(@RequestBody Customers cust)
+	{
+		repo.save(cust);
+		return cust;
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customers getCustomers4(@PathVariable("cid") int cid)
+	{
+		Customers cust=repo.getOne(cid);
+		repo.delete(cust);
+		return cust;
+	}
+	
+	@PutMapping(path="/customers", consumes= {"application/json"})
+	public Customers getCustomers5(@RequestBody Customers customers)
+	{
+		repo.save(customers);
+		return customers;
+	}
 
 }
